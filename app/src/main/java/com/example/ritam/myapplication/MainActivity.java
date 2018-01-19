b7c566d43b75791be8ae60996c95fcdc37fd0535
ritam <ritammallick_bt2k15@dtu.ac.in>
2018-01-19 15:27:09 +0530

    Added more button to the main activity, added an background image in the main activity.

--- app/src/main/java/com/example/ritam/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ritam/myapplication/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b = findViewById(R.id.button);
+        Button b = findViewById(R.id.national);
         //final RequestQueue requestQueue = Volley.newRequestQueue(this);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
